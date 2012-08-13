2e45346b7023b2ff89843b141995c19a4df3cacd
Max Tymchii <samback@ukr.net>
2012-08-13 22:54:18 +0300

    Add project folder.

---
+++ G-Organizer/G-Organizer/organizerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  organizerAppDelegate.h
+//  G-Organizer
+//
+//  Created by Max on 13.08.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface organizerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ G-Organizer/G-Organizer/organizerViewController.m
@@ -0,0 +1,34 @@
+//
+//  organizerViewController.m
+//  G-Organizer
+//
+//  Created by Max on 13.08.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#import "organizerViewController.h"
+
+@interface organizerViewController ()
+
+@end
+
+@implementation organizerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
