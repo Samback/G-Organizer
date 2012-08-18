127b2b1816f705accc9ad5e152c4bee9c68b81fc
Max Tymchii <samback@ukr.net>
2012-08-18 18:31:44 +0300

    Implements category for testing NSString.

---
+++ G-Organizer/G-Organizer/Helpers/NSString+Empty.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Empty.h
+//  G-Organizer
+//
+//  Created by Max on 18.08.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Empty)
+-(BOOL) isStringEmpty;
+@end
---
+++ G-Organizer/G-Organizer/Helpers/NSString+Empty.m
@@ -0,0 +1,16 @@
+//
+//  NSString+Empty.m
+//  G-Organizer
+//
+//  Created by Max on 18.08.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#import "NSString+Empty.h"
+
+@implementation NSString (Empty)
+-(BOOL) isStringEmpty
+{
+    return ([self isEqualToString:@""]|| self == nil) ? YES : NO;
+}
+@end
