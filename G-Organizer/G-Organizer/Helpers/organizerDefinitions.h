057356a5a6f03a34154599d1eb4c3643deda36cb
Max Tymchii <samback@ukr.net>
2012-08-18 18:29:57 +0300

    Updates definition's file.

---
+++ G-Organizer/G-Organizer/Helpers/organizerDefinitions.h
@@ -0,0 +1,20 @@
+//
+//  organizerDefenitions.h
+//  G-Organizer
+//
+//  Created by Max on 16.08.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#define APP_NAME @"G-Organizer"
+#define REQUIERD_HEADER_TITLE @"Required fields"
+#define OPTIONAL_HEADER_TITLE @"Optional fields"
+
+#define AVATAR_CELL_HEIGHT   150.0
+#define STANDART_CELL_HEIGHT 44.0
+#define TAKE_PHOTO_FROM_CAMERA @"Take avatar from camera"
+#define TAKE_PHOTO_FROM_ALBUM @"Take avatar from album"
+#define LOGIN_FIELD_IS_EMPTY @"Login field is empty, please fill it for successful registration."
+#define PASSWORD_AND_CONFIRMATION @"Password is not equal to confirm field."
+#define PASSWORD_EMPTY @"Password and confirm field could not be empty."
+#define AVATAR_EMPTY @"Please add your avatar."
